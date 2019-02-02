76501e821f998d4ec6638fc03f91007695e80a9d
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-02-02 11:32:05 -0500

    Basic tests that show components rendering but no inteded features of app yet

--- baseball-app/src/App.js
+++ baseball-app/src/App.js
@@ -2,27 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Display extends Component   {
+    render()    {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
+class Dashboard extends Component   {
+    render()    {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Display />
+        <Dashboard />
       </div>
     );
   }
 }
 
 export default App;
+export {
+    Dashboard,
+    Display
+}
--- baseball-app/src/App.test.js
+++ baseball-app/src/App.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import App from './App';
+import 'jest-dom/extend-expect';
+import App, {Display, Dashboard} from './App';
 
-it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
-  render(<App />);
-});
+
+describe('Display renders and functions properly',  ()  =>  {
+
+    test('Display without crashing', () => {
+      render(<Display />);
+    });
+    test('Display shows count of balls and strikes for at-bat', ()  =>  {
+        const component = render(<Display />);
+        const balls = component.getByTestId('balls');
+        const strikes = component.getByTestId('strikes');
+        expect(balls).toHaveContext('0');
+        expect(balls).toHaveContext('0');
+    })
+})
+
+describe('Dashboard renders and functions properly',  ()  =>  {
+
+    test('Dashboard without crashing', () => {
+      render(<Dashboard />);
+    });
+})
